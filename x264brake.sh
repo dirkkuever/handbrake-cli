6c4ee9e8e738528d7fd42738b212fc1a3cc68be5
Dirk <dirk.kuever@googlemail.com>
2022-10-17 21:42:04 +0000

    gutes Resultat und mehrere Punkte auskommentiert

--- x264brake.sh
+++ x264brake.sh
@@ -2,6 +2,7 @@
 # This is the standard path in Ubuntu Linux 22.04 LTS
 path="/usr/bin/"
 
-sudo "$path"HandBrakeCLI -i "$1" -o "$2" --format av mp4 --optimize --encoder x264 --quality 20 \
---rate 59.94 --aencoder av mp4 --ab 192 --arate 48 --width 2560 --height 1440 --verbose
+sudo "$path"HandBrakeCLI -i "$1" -o "$2" --optimize --encoder x264 --quality 22 --rate 59.94 --width 2560 --height 1440 --verbose
 
+# --format av mp4
+#  --aencoder av acc --ab 192 --arate 48
