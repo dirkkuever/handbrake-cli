ce7f54a13a414f4863ebdba4e09bc4d75903d57d
Dirk <dirk.kuever@googlemail.com>
2022-10-17 19:30:54 +0000

    arate angepasst

--- x264brake.sh
+++ x264brake.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-handbrakeCLI -i "$1" -o "$2" --format av mp4 --optimize --encoder x264 --quality 20 --rate 59.94 --aencoder av mp4 --ab 192 --arate 44.1 --width 2560 --height 1440 --verbose
+# This is the standard path in Ubuntu Linux 22.04 LTS
+path="/usr/bin/"
+
+sudo "$path"HandBrakeCLI -i "$1" -o "$2" --format av mp4 --optimize --encoder x264 --quality 20 \
+--rate 59.94 --aencoder av mp4 --ab 192 --arate 48 --width 2560 --height 1440 --verbose
 
