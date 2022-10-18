90b9d1dd157a54d5b33c73389911ea1a9eb55c11
Dirk <dirk.kuever@googlemail.com>
2022-10-18 07:45:37 +0000

    Weitere Datei für x265 ohne Vorteile

---
+++ x265brake.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# This is the standard path in Ubuntu Linux 22.04 LTS
+path="/usr/bin/"
+
+sudo "$path"HandBrakeCLI -i "$1" -o "$2" --optimize --encoder x265 --quality 22 --rate 59.94 --width 2560 --height 1440 --verbose
